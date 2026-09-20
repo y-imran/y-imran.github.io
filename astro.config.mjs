5db026c6fd541fa3e807db7886f5cc108f1b3f6d
Rehan Qadir <diracist@gmail.com>
2026-09-20 15:56:37 +0300

    migration completed to tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://y-imran.github.io',
   trailingSlash: 'always',
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '@src': '/src',
